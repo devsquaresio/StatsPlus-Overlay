01cdc3b01d549117f764a4e58a2b324df70407c5
DevSquares <111458490+devsquaresio@users.noreply.github.com>
2023-03-05 20:11:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-VERSION=0.5
+VERSION=0.51
 
 delay = 0.05
 
@@ -13,7 +13,12 @@ try:
     import json
     import sys
     import tkinter
+    import logging
     os.system('cls' if os.name == 'nt' else 'clear')
+
+    logging.basicConfig(level=logging.DEBUG, format="{asctime} {levelname:<8} {message}", style='{', filename='debug.log', filemode='a')
+
+
 
     def install_package(pk):
         os.system(f"{sys.executable} -m pip install {pk}")
@@ -34,6 +39,7 @@ try:
         import customtkinter as ctk
 except:
     print("There was an error importing the required libraries. The program will most likely encounter errors.")
+    logging.critical("PACKAGES WEREN'T ABLE TO BE IMPORTED PROPERLY")
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -51,9 +57,11 @@ if latest_overlay_code.status_code == 200:
                 f.write(latest_overlay_code)
             time.sleep(1)
             print("Update complete! Re-run the app.")
+            logging.info(f"Updated. {VERSION} -> {latest_version}")
             input()
         else:
             print("Ignored update.")
+            logging.debug(f"Update version {latest_version} ignored.")
 
 time.sleep(2)
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -102,12 +110,16 @@ def check_logs(empty_client_list: list) -> list:
 
     if os.path.exists(bd_logs):
         empty_client_list.append("Badlion")
+        logging.debug("Badlion client found on computer.")
     if os.path.exists(lunar_logs):
         empty_client_list.append("Lunar")
+        logging.debug("Lunar client found on computer.")
     if os.path.exists(olunar_logs):
         empty_client_list.append("Lunar")
+        logging.debug("Lunar client found on computer.")
     if os.path.exists(vn_logs):
         empty_client_list.append("Vanilla")
+        logging.debug("Regular Minecraft found on computer.")
 
 # Thanks kopamed
 def fix_line(line):
@@ -128,21 +140,28 @@ client_choice = input("> ")
 if client_choice == "1":
     if os.path.exists(bd_logs):
         path = bd_logs
+        logging.debug("Badlion client logs had been added")
     else:
         print("Log not found")
+        logging.error("Badlion client not found on computer.")
     
 elif client_choice == "2":
     if os.path.exists(lunar_logs):
         path = lunar_logs
+        logging.debug("Lunar client logs had been added")
     elif os.path.exists(olunar_logs):
         path = olunar_logs
+        logging.debug("Lunar client logs had been added")
     else:
         print("Log not found")
+        logging.error("Lunar client logs not found on computer.")
 elif client_choice == "3":
     if os.path.exists(vn_logs):
         path = vn_logs
+        logging.debug("Regular Minecraft logs had been added")
     else:
         print("Log not found")
+        logging.error("Vanilla client logs not found on computer.")
 elif client_choice == "4":
     # Check until the path exists
     while True:
@@ -150,9 +169,11 @@ elif client_choice == "4":
         log_path = input("> ")
         if os.path.exists(log_path):
             print("Log Found!")
+            logging.info("Logs found on computer.")
             break
         else:
             print("Log not found. Try again!\n")
+            logging.warning("Log not found.")
     path = log_path
 
 time.sleep(2)
@@ -169,6 +190,7 @@ if os.path.exists("apikey.txt"):
         #print(api_key)
         if api_key == "":
             print(r"Api Key NOT found. Type /api new.")
+            logging.error("Api key not found")
             # keeps reading until api key found
             while True:
                 with open(path, "r") as h:
@@ -178,15 +200,18 @@ if os.path.exists("apikey.txt"):
                         with open("apikey.txt", "w") as i:
                             i.write(api_key)
                             print("Api Key found. Join a game of bedwars to try the overlay!")
+                            logging.info("Api key found")
                             break
                 time.sleep(delay)
         else:
             print("Api Key found. Join a game of bedwars to try the overlay!")
+            logging.info("Api Key saved in file already")
 
 # If file doesn't exist, then create it and ask to give api key
 else:
     with open("apikey.txt", "x") as f:
         print(r"Api Key NOT found. Type /api new.")
+        logging.error("Api key not found")
         while True:
             with open(path, "r") as i:
                 latest_line = fix_line(i.readlines()[-1])
@@ -195,6 +220,7 @@ else:
                     with open("apikey.txt", "w") as j:
                         j.write(api_key)
                         print("Api Key found. Join a game of bedwars to try the overlay!")
+                        logging.info("Api key found")
                         break
             time.sleep(delay)
     
@@ -226,6 +252,7 @@ def update_stats(queue, player_list, api_key):
             # check if invalid
             if data == "NULL":
                 print(f'{player}: UNABLE TO FETCH STATS')
+                logging.debug(f"Unable to fetch stats for {player}")
             # Check if nick
             elif data == "NICKED":
                 print(f'{player}: NICKED')
@@ -239,9 +266,11 @@ def update_stats(queue, player_list, api_key):
             if response.status_code == 204:
                 print(f"{player}: NICKED")
                 player_list[player] = "NICKED"
+                logging.debug(f"{player} was nicked.")
             else:
                 try:
                     uuid = response.json()['id']
+                    logging.debug(f"{player} has uuid {uuid}")
                     # Use api key to help grab data
                     requestlink = str(f"https://api.hypixel.net/player?key={api_key}&uuid=" + uuid)
                     hydata = requests.get(requestlink).json()
@@ -290,10 +319,12 @@ def update_stats(queue, player_list, api_key):
 
                     player_list[player] = [rank, lvl, winstreak, wl, kd, fkd] # Makes it easy to grab data
                     print(f"[{rank}]{player}, {lvl}: [WINSTREAK: {winstreak}], [WLR: {wl}], [KDR: {kd}], [FKD: {fkd}]")
+                    logging.debug(f"Stats for {player} were given.")
                     # Todo: Add more things like party                
                 except:
                     player_list[player] = "NICK"
                     print(f"{player}: NICKED")
+                    logging.debug(f"{player} was NICKED.")
         
 
 
@@ -306,6 +337,7 @@ while True:
             latest_line = fix_line(f.readlines()[readlen])
             if " has quit!" in latest_line: # Check if player left
                 name = latest_line.split(" ")[0]
+                logging.debug(f"{name} has left the bedwars lobby")
                 queue.remove(name)
                 update_stats(queue, player_list, api_key)
             elif latest_line.lower().startswith("online: "): # /who SHOULD ONLY BE RAN ONCE
@@ -313,12 +345,14 @@ while True:
                 player_list = {}
                 for player in latest_line.strip("ONLINE: ").split(", "):
                     queue.append(player)
+                logging.debug("New game has started")
                 
                 update_stats(queue, player_list, api_key)
             elif " has joined" in latest_line.lower(): # Check if player joined
                 name = latest_line.split(" ")[0]
                 if name not in queue:
                     queue.append(name)
+                logging.debug(f"{name} joined the bedwars lobby")
                 update_stats(queue, player_list, api_key)
             elif "protect your bed and destroy the enemy beds." in latest_line.lower():
                 # Todo: Handle minimization
@@ -326,15 +360,23 @@ while True:
             elif "has disconnected" in latest_line: # Check if player left mid-game idk why
                 name = latest_line.split(" ")[0]
                 queue.remove(name)
+                logging.debug(f"{name} left in the middle of the game")
+                update_stats(queue, player_list, api_key)
+            elif "reconnected" in latest_line: # Check if player left mid-game idk why
+                name = latest_line.split(" ")[0]
+                queue.append(name)
+                logging.debug(f"{name} rejoined in the middle of the game")
                 update_stats(queue, player_list, api_key)
             
             elif latest_line.startswith("Your new API key is"): # Check if player changed api-key midway
                 api_key = latest_line.split(" ")[-1]
                 with open("apikey.txt", "w") as f:
                     f.write(api_key)
+                logging.info(f"{name} new API key found!")
             readlen += 1
         except:
             pass # just doesn't work? 
+            logging.critical("Failure in reading the logs.")
         
         
 
